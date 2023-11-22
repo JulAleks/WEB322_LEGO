75c41d4af8e8a88fe66abd4feacd3f66b8161a0b
JulAleks <jalekseev@myseneca.ca>
2023-11-22 13:46:09 -0500

    update multer

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -6,7 +6,7 @@
  *
  * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
  *
- * Name: Julia Alekseev Student ID: 051292134 Date: Nov 19, 2023
+ * Name: Julia Alekseev Student ID: 051292134 Date: Nov 22, 2023
  *
  * Published URL: https://dull-red-lovebird-shoe.cyclic.app/
  ********************************************************************************/
